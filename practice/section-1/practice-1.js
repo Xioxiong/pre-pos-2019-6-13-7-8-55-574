c8d43c1187b1872a9e409a8a05b9000df16adf61
xiongwei <2894001817@qq.com>
2019-08-21 10:49:39 +0800

    练习1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  let temp = [];
+  for(let i=0;i<collectionA.length;i++){
+    for(let j=0;j<collectionB.length;j++){
+      if(collectionA[i] === collectionB[j]){
+        temp.push(collectionA[i]);
+      }
+    }
+  }
+  return temp;
 }
